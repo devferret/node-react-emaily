640a7c5041dc6eedb6fe390927f9767ed9abe8f5
devferret <warunyu.pon@gmail.com>
2017-10-26 14:58:40 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+// Express path and adjust request, response
+app.get('/greeting', (req, res) => {
+    res.send({ hello : 'world' })
+})
+
+// Dynamic PORT binding
+// Define PORT provide from HUROKU or 5000 by default
+const PORT = process.env.PORT || 5000
+
+// Excute express app
+app.listen(5000)
