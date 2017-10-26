d3f6bb1849ee56313a8ed219869425b74b7f2235
devferret <warunyu.pon@gmail.com>
2017-10-26 15:24:39 +0700

    change port

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 
 // Express path and adjust request, response
-app.get('/greeting', (req, res) => {
+app.get('/', (req, res) => {
     res.send({ hello : 'world' })
 })
 
 // Dynamic PORT binding
 // Define PORT provide from HUROKU or 5000 by default
-const PORT = process.env.PORT || 5000
+const PORT = (process.env.PORT || 5000)
 
 // Excute express app
-app.listen(5000)
+app.listen(PORT)
